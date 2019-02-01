66661c6a96948fc73fef9ecf822d71ee195848c1
Sara Ersson <saraers@kth.se>
2019-02-01 16:18:00 +0100

    feat: add unit tests for the pipeline

--- src/main/java/ci/Pipeline.java
+++ src/main/java/ci/Pipeline.java
@@ -3,6 +3,9 @@ package ci;
 import org.apache.commons.io.FileUtils;
 import org.eclipse.jgit.api.Git;
 import org.eclipse.jgit.api.errors.GitAPIException;
+import org.eclipse.jgit.api.errors.InvalidRemoteException;
+import org.eclipse.jgit.api.errors.JGitInternalException;
+import org.eclipse.jgit.api.errors.TransportException;
 
 import java.io.File;
 import java.io.IOException;
@@ -12,38 +15,74 @@ import java.util.concurrent.TimeUnit;
 public class Pipeline {
 
 
+    /**
+     * git object used by JGit
+     */
     private Git git;
+    /**
+     * URL of the repository to clone
+     */
+    private String repoUrl;
+    /**
+     * unique id of the commit to build
+     */
+    private String commitSha;
+    /**
+     * directory  in which the directory  will be cloned
+     */
+    private String localWorkingDirectory;
+
 
     /**
      * @param repoUrl
-     * @param localDirectoryPath
-     * @return
-     * @throws CloneException if an error happens
+     * @param commitSha
+     * @param localWorkingDirectory
      */
-    public void cloneRepository(String repoUrl, String localDirectoryPath) throws CloneException {
-        try {
-            Git git = Git.cloneRepository()
-                    .setURI(repoUrl)
-                    .setDirectory(new File(localDirectoryPath))
-                    .call();
-            // store the git object in class variable
-            this.git = git;
-        } catch (GitAPIException e) {
-            e.printStackTrace();
-            throw new CloneException();
-        }
+    public Pipeline(String repoUrl, String commitSha, String localWorkingDirectory) {
+        this.repoUrl = repoUrl;
+        this.commitSha = commitSha;
+        this.localWorkingDirectory = localWorkingDirectory;
+
     }
 
 
     /**
-     * @param commitSha commitSha unique id of the commit
-     * @throws CheckoutException
+     * @return the root directory of the repo
      */
-    public void checkoutRepo(String commitSha) throws CheckoutException {
+    public File getRepoDirectory() {
+        return this.git.getRepository().getDirectory().getParentFile();
+    }
+
+
+    /**
+     * @throws CloneException if an errors happens
+     */
+    public void cloneRepository() throws CloneException {
+
+        Git git = null;
         try {
-            this.git.checkout().setName(commitSha).call();
+            git = Git.cloneRepository()
+                    .setURI(this.repoUrl)
+                    // use sha of the commit to have unique directories
+                    .setDirectory(new File(this.localWorkingDirectory + "/" + this.commitSha))
+                    .call();
+        } catch (GitAPIException | JGitInternalException e) {
+            throw new CloneException(e.getCause());
+        }
+        // store the git object in class variable
+        this.git = git;
+
+    }
+
+
+    /**
+     * @throws CheckoutException if an error happens
+     */
+    public void checkoutRepo() throws CheckoutException {
+        try {
+            this.git.checkout().setName(this.commitSha).call();
         } catch (GitAPIException e) {
-            throw new CheckoutException();
+            throw new CheckoutException(e.getCause());
         }
     }
 
@@ -51,14 +90,14 @@ public class Pipeline {
     /**
      * @param compileCommand string of the command to compile (e.g "mvn compile")
      * @param waitForSecond  max number of seconds a process can execute
-     * @return
+     * @return true if it compiles, false if not
      */
     public boolean compileRepo(String compileCommand, int waitForSecond) throws InterruptedException, IOException {
         String[] commandArray = compileCommand.split(" ");
         ProcessBuilder compileProcessBuilder = null;
         try {
             compileProcessBuilder =
-                    new ProcessBuilder().command(commandArray).inheritIO().directory(this.git.getRepository().getDirectory());
+                    new ProcessBuilder().command(commandArray).inheritIO().directory(this.getRepoDirectory());
 
 
             Process compileProcess = compileProcessBuilder.start();
@@ -75,16 +114,16 @@ public class Pipeline {
 
 
     /**
-     * @param testCommand   string of the command to compile
+     * @param testCommand   string of the command to test
      * @param waitForSecond max number of seconds a process can execute
-     * @return
+     * @return true if it passes tests, false if not
      */
     public boolean testRepo(String testCommand, int waitForSecond) throws InterruptedException, IOException {
         String[] commandArray = testCommand.split(" ");
         ProcessBuilder testProcessBuilder = null;
         try {
             testProcessBuilder =
-                    new ProcessBuilder().command(commandArray).inheritIO().directory(this.git.getRepository().getDirectory());
+                    new ProcessBuilder().command(commandArray).inheritIO().directory(this.getRepoDirectory());
 
 
             Process testProcess = testProcessBuilder.start();
@@ -99,9 +138,9 @@ public class Pipeline {
 
     }
 
-    public void clear(){
+    public void clear() {
         try {
-            FileUtils.deleteDirectory(this.git.getRepository().getDirectory());
+            FileUtils.deleteDirectory(this.git.getRepository().getDirectory().getParentFile());
             this.git = null;
         } catch (IOException e) {
             e.printStackTrace();
--- src/test/java/ci/PipelineTest.java
+++ src/test/java/ci/PipelineTest.java
@@ -1,46 +1,165 @@
 package ci;
 
-import org.junit.jupiter.api.BeforeAll;
-import org.junit.jupiter.api.Test;
+import org.apache.commons.io.FileUtils;
+import org.junit.jupiter.api.*;
+
+import java.io.File;
+import java.io.IOException;
 
 import static org.junit.jupiter.api.Assertions.*;
 
 class PipelineTest {
 
 
-
-    static private String goodUrl = "https://github.com/Cpptz/dd22480_lab_2";
-    static private String badUrl = "https://github.com/Cpptz/dd224";
-    String directoryPath = ".";
-
+    String goodUrl = "https://github.com/Cpptz/dd22480_lab_1";
+    String badUrl = "https://github.com/Cpptz/dd224";
+    static String directoryPath = "./repos";
 
 
+    @BeforeAll
+    static void initAll() {
+        new File(directoryPath).mkdir();
+    }
+
+    @AfterAll
+    static void tearDownAll(){
+        try {
+            FileUtils.deleteDirectory(new File(directoryPath));
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+
+    }
 
     @Test
     void cloneRepository() {
 
 
-//        // use a wrong url
-//        assertThrows(CloneException.class, () -> {
-//            pipeline.cloneRepository(badUrl,);
-//        });
-//
-//
-//        pipeline.cloneRepository(goodUrl);
+        // use a wrong url
+        Pipeline pipeline = new Pipeline(badUrl, "9ed9801", directoryPath);
+
+        // this should throw an exception
+        assertThrows(CloneException.class, () -> {
+            pipeline.cloneRepository();
+        });
 
 
+
+
+        // test that it doesn't throw any exceptions
+        Pipeline pipeline1 = new Pipeline(goodUrl, "9ed9801", directoryPath);
+
+        try {
+            pipeline1.cloneRepository();
+
+            // we should never pass here
+        } catch (CloneException e) {
+            e.printStackTrace();
+            Assertions.fail();
+        }
+        // delete repo
+        pipeline1.clear();
 
     }
 
     @Test
     void checkoutRepo() {
+
+        // valid url, wrong commitsha
+        Pipeline pipeline1 = new Pipeline(goodUrl, "8geg7ze", directoryPath);
+        try {
+            pipeline1.cloneRepository();
+            // this should throw an exception
+            assertThrows(CheckoutException.class, () -> {
+                pipeline1.checkoutRepo();
+            });
+
+            // we should never pass here
+        } catch (CloneException e) {
+            e.printStackTrace();
+            Assertions.fail();
+        }
+        // delete repo
+        pipeline1.clear();
+
+
+        // valid url, valid commitSha
+        Pipeline pipeline2 = new Pipeline(goodUrl, "9ed9801", directoryPath);
+        Exception ex = null;
+        try {
+            pipeline2.cloneRepository();
+            // this should throw an exception
+            pipeline2.checkoutRepo();
+
+
+            // we should never pass here
+        } catch (CloneException | CheckoutException e) {
+            Assertions.fail();
+        }
+
+        pipeline2.clear();
+
+
     }
 
     @Test
     void compileRepo() {
+        Pipeline pipeline = new Pipeline(goodUrl, "9ed9801", directoryPath);
+
+        // valid url, valid sha + valid code => should compile : return true (no exceptions should be thrown)
+        try {
+            pipeline.cloneRepository();
+            pipeline.checkoutRepo();
+            assertTrue(pipeline.compileRepo("mvn compile -q", 10));
+
+            // we should never pass here
+        } catch (CloneException | CheckoutException | InterruptedException | IOException e) {
+            e.printStackTrace();
+            Assertions.fail();
+        }
+
+        // valid url, valid sha + invalid code => should not compile : return false (no exceptions should be thrown)
+        File pom = new File(pipeline.getRepoDirectory() + "/pom.xml");
+        pom.delete();
+
+        try {
+            assertFalse(pipeline.compileRepo("mvn compile -q", 10));
+        } catch (InterruptedException | IOException e) {
+            e.printStackTrace();
+        }
+
+        // delete repo
+        pipeline.clear();
+
     }
 
     @Test
     void testRepo() {
+        Pipeline pipeline = new Pipeline(goodUrl, "9ed9801", directoryPath);
+
+        // valid url, valid sha + valid code => should pass tests : return true (no exceptions should be thrown)
+        try {
+            pipeline.cloneRepository();
+            pipeline.checkoutRepo();
+            assertTrue(pipeline.testRepo("mvn test -q", 10));
+
+            // we should never pass here
+        } catch (CloneException | CheckoutException | InterruptedException | IOException e) {
+            Assertions.fail();
+        }
+
+
+        // valid url, valid sha + invalid code => should not pass tests : return false (no exceptions should be thrown)
+        File pom = new File(pipeline.getRepoDirectory() + "/pom.xml");
+        pom.delete();
+        try {
+            assertFalse(pipeline.testRepo("mvn test -q", 10));
+        } catch (InterruptedException | IOException e) {
+            Assertions.fail();
+        }
+
+        // delete repo
+        pipeline.clear();
+
     }
 }
