c6672b05434851b5fa778b3a56f1c5de3fd91426
rgunning <rgunning@kth.se>
2019-02-07 14:57:15 +0100

    fix: fixed the url the other way around, the right way

--- src/main/java/ci/Parser.java
+++ src/main/java/ci/Parser.java
@@ -13,7 +13,7 @@ class Parser {
 			c.ref = jelement.getAsJsonObject().get("ref").getAsString();
 			JsonArray commits = jelement.getAsJsonObject().get("commits").getAsJsonArray();
 			c.sha = commits.get(0).getAsJsonObject().get("id").getAsString();
-			c.url = ("commit/" + commits.get(0).getAsJsonObject().get("id").getAsString());
+			c.url = commits.get(0).getAsJsonObject().get("url").getAsString().substring(0,39);
 			c.message = commits.get(0).getAsJsonObject().get("message").getAsString();
 			c.timestamp = commits.get(0).getAsJsonObject().get("timestamp").getAsString();
 			c.name = commits.get(0).getAsJsonObject().get("author").getAsJsonObject().get("name").getAsString();
