badc20378819095bb776c719981cb98c0f19ebb2
Cpptz <Cpptz@users.noreply.github.com>
2019-02-08 01:25:19 +0100

    Update CheckoutException.java

--- src/main/java/ci/CheckoutException.java
+++ src/main/java/ci/CheckoutException.java
@@ -2,7 +2,7 @@ package ci;
 
 public class CheckoutException extends Exception {
     public CheckoutException() { super(); }
-    public CheckoutException(String message) { super(message); }
+    public CheckoutException(String me) { super(message); }
     public CheckoutException(String message, Throwable cause) { super(message, cause); }
     public CheckoutException(Throwable cause) { super(cause); }
 }
